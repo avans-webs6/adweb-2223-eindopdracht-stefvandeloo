7b7071060067fd3a5e2367b8756bb9cd5024f5e8
smajwloo <smajw.vandeloo@student.avans.nl>
2023-08-01 15:54:04 +0200

    Add category name to transaction

--- Huishoudboekjes-app/src/app/huishoudboekjes-detail-transactions/huishoudboekjes-detail-transactions.component.ts
+++ Huishoudboekjes-app/src/app/huishoudboekjes-detail-transactions/huishoudboekjes-detail-transactions.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Transaction } from '../transaction.model';
 import { TransactionType } from '../transaction-type.enum';
+import { CategoryService } from '../category.service';
+import { Category } from '../category.model';
 
 @Component({
   selector: 'app-huishoudboekjes-detail-transactions',
@@ -14,6 +16,20 @@ export class HuishoudboekjesDetailTransactionsComponent {
   @Input()
   transactions: Transaction[] = [];
 
+  categories: Category[] = [];
+
   @Input()
   transactionType = TransactionType.INCOME;
+
+  constructor(private categoryService: CategoryService) { 
+    this.categoryService.getCategories().subscribe(categories => {
+      this.categories = categories;
+    });
+  }
+
+  getCategoryName(categoryId: string) {
+    let category = this.categories.filter(c => c.id === categoryId)[0];
+    if (!category) return "";
+    return category.name;
+  }
 }
