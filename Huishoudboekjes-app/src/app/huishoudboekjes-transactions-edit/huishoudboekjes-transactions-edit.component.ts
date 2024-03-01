97a4a2e592f3bb99146398c727a18da7d0bb3b2a
smajwloo <smajw.vandeloo@student.avans.nl>
2024-03-01 14:50:02 +0100

    Edit transaction validation

--- Huishoudboekjes-app/src/app/huishoudboekjes-transactions-edit/huishoudboekjes-transactions-edit.component.ts
+++ Huishoudboekjes-app/src/app/huishoudboekjes-transactions-edit/huishoudboekjes-transactions-edit.component.ts
@@ -4,6 +4,7 @@ import { TransactionService } from '../transaction.service';
 import { TransactionType } from '../transaction-type.enum';
 import { Category } from '../category.model';
 import { CategoryService } from '../category.service';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-huishoudboekjes-transactions-edit',
@@ -22,8 +23,15 @@ export class HuishoudboekjesTransactionsEditComponent {
   transaction: Transaction = new Transaction();
 
   editDialog: any;
+  editTransactionForm: FormGroup;
 
-  constructor (private transactionService: TransactionService, private categoryService: CategoryService) {  }
+  constructor (private transactionService: TransactionService, private categoryService: CategoryService) {
+    this.editTransactionForm = new FormGroup({
+      'price': new FormControl(null, [Validators.required, Validators.min(0.01)]),
+      'description': new FormControl(null),
+      'category': new FormControl(null)
+    });
+  }
 
   ngAfterViewInit(): void {
     this.editDialog = document.getElementById("edit-transaction-dialog-" + this.transactionId) as HTMLDialogElement;
@@ -36,19 +44,30 @@ export class HuishoudboekjesTransactionsEditComponent {
   openEditDialog() {
     this.transactionService.getTransaction(this.transactionId).subscribe((transaction) => {
       this.transaction = transaction;
+      this.editTransactionForm.setValue({price: transaction.price, description: transaction.description, category: transaction.categoryId});
     });
-    
+
     this.editDialog.showModal();
   }
 
   onSave() {
-    if (this.transaction.description && this.transaction.price) {
-      this.transactionService.editTransaction(this.transaction);
-      this.editDialog.close();
-    }
+    if (this.validatePrice()) return;
+    this.transaction.price = this.editTransactionForm.value.price;
+    this.transaction.description = this.editTransactionForm.value.description;
+    this.transaction.categoryId = this.editTransactionForm.value.category;
+    this.transactionService.editTransaction(this.transaction);
+    this.editDialog.close();
   }
 
   onCancel() {
     this.editDialog.close();
+  }
+
+  validatePrice() {
+    const priceFormItem = this.editTransactionForm.get('price');
+
+    if (!priceFormItem) return;
+    if (!priceFormItem.value || priceFormItem.errors?.['min']) return "Please enter a price greater than 0.01";
+    return;
   }
 }
