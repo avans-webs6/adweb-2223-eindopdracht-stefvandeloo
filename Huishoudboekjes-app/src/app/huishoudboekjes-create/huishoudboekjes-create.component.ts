a7111f22ddd79c65db910c17a332c46c3d960e52
smajwloo <smajw.vandeloo@student.avans.nl>
2024-03-01 13:32:50 +0100

    Create book validation

--- Huishoudboekjes-app/src/app/huishoudboekjes-create/huishoudboekjes-create.component.ts
+++ Huishoudboekjes-app/src/app/huishoudboekjes-create/huishoudboekjes-create.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Book } from '../book.model';
 import { BookService } from '../book.service';
 import {getAuth} from "firebase/auth";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-huishoudboekjes-create',
@@ -13,9 +14,16 @@ export class HuishoudboekjesCreateComponent {
   book: Book;
   createDialog: any;
 
+  createBookForm: FormGroup;
+
   constructor(public bookService: BookService) {
     const auth = getAuth();
     this.book = new Book(auth.currentUser?.email ?? "");
+
+    this.createBookForm = new FormGroup({
+      'title': new FormControl(null, [Validators.required]),
+      'description': new FormControl(null)
+    });
   }
 
   ngOnInit(): void {
@@ -31,13 +39,22 @@ export class HuishoudboekjesCreateComponent {
   }
 
   onSave() {
-    if (!this.book.title || !this.book.description) return;
-
+    if (this.validateTitle()) return;
+    this.book.createBook(this.book.id, this.createBookForm.value.title, this.createBookForm.value.description);
     this.bookService.addBook(this.book);
+    this.createBookForm.reset();
     this.createDialog.close();
   }
 
   onCancel() {
     this.createDialog.close();
+  }
+
+  validateTitle() {
+    const titleFormItem = this.createBookForm.get('title');
+
+    if (!titleFormItem) return;
+    if (!titleFormItem.value) return "Please enter a title";
+    return;
   }
 }
