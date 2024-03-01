f87c1d3564558b68186379a76bd1effedc47d522
smajwloo <smajw.vandeloo@student.avans.nl>
2024-03-01 14:43:12 +0100

    Create transaction validation

--- Huishoudboekjes-app/src/app/huishoudboekjes-transactions-create/huishoudboekjes-transactions-create.component.ts
+++ Huishoudboekjes-app/src/app/huishoudboekjes-transactions-create/huishoudboekjes-transactions-create.component.ts
@@ -5,6 +5,7 @@ import { TransactionType } from '../transaction-type.enum';
 import { DatePipe } from '@angular/common';
 import { CategoryService } from '../category.service';
 import { Category } from '../category.model';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-huishoudboekjes-transactions-create',
@@ -21,10 +22,17 @@ export class HuishoudboekjesTransactionsCreateComponent {
 
   categories: Category[] = [];
   transaction: Transaction = new Transaction();
-  
-  createDialog: any;
 
-  constructor(public transactionService: TransactionService, private categoryService: CategoryService, public datepipe: DatePipe) {  }
+  createDialog: any;
+  createTransactionForm: FormGroup;
+
+  constructor(public transactionService: TransactionService, private categoryService: CategoryService, public datepipe: DatePipe) {
+    this.createTransactionForm = new FormGroup({
+      'price': new FormControl(null, [Validators.required, Validators.min(0.01)]),
+      'description': new FormControl(null),
+      'category': new FormControl(null)
+    });
+  }
 
   ngAfterViewInit(): void {
     this.createDialog = document.getElementById("create-" + this.transactionType + "-dialog") as HTMLDialogElement;
@@ -35,15 +43,19 @@ export class HuishoudboekjesTransactionsCreateComponent {
   }
 
   onSave() {
-    if (this.transaction.description && this.transaction.price) {
-      this.transaction.bookId = this.bookId;
-      this.transaction.type = this.transactionType;
-      this.transaction.date = this.createDateStringOfToday();
-      this.transactionService.addTransaction(this.transaction);
+    if (this.validatePrice()) return;
+    this.transaction.createTransaction(this.transaction.id,
+                                      this.createTransactionForm.value.description,
+                                      this.createTransactionForm.value.price,
+                                      this.createDateStringOfToday(),
+                                      this.transactionType,
+                                      this.bookId,
+                                      this.createTransactionForm.value.category);
+    this.transactionService.addTransaction(this.transaction);
 
-      this.transaction = new Transaction();
-      this.createDialog.close();
-    }
+    this.transaction = new Transaction();
+    this.createTransactionForm.reset();
+    this.createDialog.close();
   }
 
   onCancel() {
@@ -54,5 +66,13 @@ export class HuishoudboekjesTransactionsCreateComponent {
   createDateStringOfToday() {
     let date = this.datepipe.transform(new Date(), 'yyyy-MM-dd');
     return date ? date : new Date().toString();
+  }
+
+  validatePrice() {
+    const priceFormItem = this.createTransactionForm.get('price');
+
+    if (!priceFormItem) return;
+    if (!priceFormItem.value || priceFormItem.errors?.['min']) return "Please enter a price greater than 0.01";
+    return;
   }
 }
