a683b34cc24d6b2820e536fda0e6ecbabc543f50
smajwloo <smajw.vandeloo@student.avans.nl>
2024-03-20 14:10:42 +0100

    Add form validation category create & edit

--- Huishoudboekjes-app/src/app/huishoudboekjes-categories-create/huishoudboekjes-categories-create.component.ts
+++ Huishoudboekjes-app/src/app/huishoudboekjes-categories-create/huishoudboekjes-categories-create.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Category } from '../category.model';
 import { CategoryService } from '../category.service';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-huishoudboekjes-categories-create',
@@ -11,8 +12,15 @@ export class HuishoudboekjesCategoriesCreateComponent {
 
   category: Category = new Category();
   createDialog: any;
+  createCategoryForm: FormGroup;
 
-  constructor(public categoryService: CategoryService) {  }
+  constructor(public categoryService: CategoryService) {
+    this.createCategoryForm = new FormGroup({
+      'name': new FormControl(null, [Validators.required]),
+      'budget': new FormControl(null, [Validators.required, Validators.min(0.01)]),
+      'date': new FormControl(null)
+    });
+  }
 
   ngOnInit(): void {
     this.createDialog = document.getElementById("create-category-dialog") as HTMLDialogElement;
@@ -24,13 +32,47 @@ export class HuishoudboekjesCategoriesCreateComponent {
   }
 
   async onSave() {
-    if (this.category.name && this.category.budget) {
-      await this.categoryService.addCategory(this.category);
-      this.createDialog.close();
-    }
+    if (this.validateForm()) return;
+
+    const budget = this.createCategoryForm.value.budget.toFixed(2);
+    this.category.createCategory(this.category.id,
+                                this.createCategoryForm.value.name,
+                                budget);
+    this.category.setEndDate(this.createCategoryForm.value.date);
+    await this.categoryService.addCategory(this.category);
+    this.createCategoryForm.reset();
+    this.createDialog.close();
   }
 
   onCancel() {
     this.createDialog.close();
   }
+
+  validateForm() {
+      return this.validateName() || this.validateBudget() || this.validateDate();
+  }
+
+    validateName() {
+        const title = this.createCategoryForm.get('name');
+
+        if (!title) return;
+        if (!title.value) return "Please enter a name";
+        return;
+    }
+
+    validateBudget() {
+        const budget = this.createCategoryForm.get('budget');
+
+        if (!budget) return;
+        if (!budget.value || budget.errors?.['min']) return "Please enter a budget greater than 0.01";
+        return;
+    }
+
+    validateDate() {
+        const date = this.createCategoryForm.get('date');
+
+        if (!date) return;
+        if (date.value && new Date(date.value).getTime() < new Date().getTime()) return "Please enter a date in the future";
+        return;
+    }
 }
--- Huishoudboekjes-app/src/app/huishoudboekjes-categories-edit/huishoudboekjes-categories-edit.component.ts
+++ Huishoudboekjes-app/src/app/huishoudboekjes-categories-edit/huishoudboekjes-categories-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Category } from '../category.model';
 import { CategoryService } from '../category.service';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-huishoudboekjes-categories-edit',
@@ -13,8 +14,15 @@ export class HuishoudboekjesCategoriesEditComponent {
   category: Category = new Category();
 
   editDialog: any;
+  editCategoryForm: FormGroup;
 
-  constructor (private categoryService: CategoryService) {  }
+  constructor (private categoryService: CategoryService) {
+      this.editCategoryForm = new FormGroup({
+          'name': new FormControl(null, [Validators.required]),
+          'budget': new FormControl(null, [Validators.required, Validators.min(0.01)]),
+          'date': new FormControl(null)
+      });
+  }
 
   ngAfterViewInit(): void {
     this.editDialog = document.getElementById("edit-category-dialog-" + this.category.id) as HTMLDialogElement;
@@ -23,19 +31,52 @@ export class HuishoudboekjesCategoriesEditComponent {
   openEditDialog() {
     this.categoryService.getCategory(this.category.id).subscribe((category) => {
       this.category = category;
+      this.editCategoryForm.setValue({name: category.name, budget: category.budget, date: category.endDate});
     });
 
     this.editDialog.showModal();
   }
 
   async onSave() {
-    if (this.category.name && this.category.budget) {
+      if (this.validateForm()) return;
+
+      this.category.name = this.editCategoryForm.value.name;
+      this.category.budget = this.editCategoryForm.value.budget;
+      this.category.endDate = this.editCategoryForm.value.date;
       await this.categoryService.editCategory(this.category);
+      this.editCategoryForm.reset();
       this.editDialog.close();
-    }
   }
 
   onCancel() {
     this.editDialog.close();
   }
+
+    validateForm() {
+        return this.validateName() || this.validateBudget() || this.validateDate();
+    }
+
+    validateName() {
+        const title = this.editCategoryForm.get('name');
+
+        if (!title) return;
+        if (!title.value) return "Please enter a name";
+        return;
+    }
+
+    validateBudget() {
+        const budget = this.editCategoryForm.get('budget');
+
+        if (!budget) return;
+        if (!budget.value || budget.errors?.['min']) return "Please enter a budget greater than 0.01";
+        return;
+    }
+
+    validateDate() {
+        const date = this.editCategoryForm.get('date');
+
+        if (!date) return;
+        if (date.value && new Date(date.value).getTime() < new Date().getTime()) return "Please enter a date in the future";
+        return;
+    }
 }
